08d4a013b9ac9fbec99a0f12ea2f09098b6e7fb1
SukaiKitsume@gmail.com <SukaiKitsume@gmail.com@b4266907-eba7-20e5-f0ae-2bf237ab4c38>
2012-05-27 21:37:50 +0000

    +Add spreadsheets -delete old files

--- trunk/Engine/Source/Managers/GameObjectsManager/BaseGameObjects.h
+++ trunk/Engine/Source/Managers/GameObjectsManager/BaseGameObjects.h
@@ -40,6 +40,8 @@ class BaseGameObjects
 		{
 			return "BaseGameObject";
 		}
+		
+
 
 };
 
---
+++ trunk/Engine/Source/Managers/StringManager/spreadsheets/SpreadSheetsColumn.h
@@ -0,0 +1,16 @@
+#ifndef GAME_SpreadSheetsColumn_H
+#define GAME_SpreadSheetsColumn_H
+
+
+class SpreadSheetsColumn
+{
+	public:
+		/** Default constructor */
+		SpreadSheetsColumn();
+		/** Default destructor */
+		virtual ~SpreadSheetsColumn();
+	protected:
+	private:
+};
+
+#endif // GAME_SpreadSheetsColumn_H
